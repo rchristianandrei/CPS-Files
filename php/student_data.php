abec4a9be393cdaa64bf2b4ef63014626a5082c3
rchristian <98152145+rchristianandrei@users.noreply.github.com>
2022-02-25 15:42:34 +0800

    updated student_data.php

--- php/student_data.php
+++ php/student_data.php
@@ -44,12 +44,16 @@
     </head>
     <body>
         <header>
-            <h1>CPS-Laguna</h1>
+            <img src="../images/cps-logo.png" alt="cps logo" class="logo">
             <nav>
-                <span><a href="index.html">Home</a></span>
-                <span>Query</span>
-                <span>Profile</span>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
             </nav>
+            <button><a href="index.html">Login</a></button>
         </header>
         <main>
             <table>
@@ -107,7 +111,57 @@
             </table>
         </main>
         <footer>
-
+            <div>
+                <img src="../images/footer-logo.jpg" alt="cps logo" class="logo"> 
+                <hr>
+                <div style="display: flex;">
+                    <span class="news">
+                        <span>Subscribe to our newsletter</span>
+                    </span>
+                    <span class="about">
+                        <section class="section">
+                            <ul>
+                                <caption>Site Map</caption>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Events</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </section>
+                        <section class="section">
+                            <ul>
+                                <caption>About</caption>
+                                <li><a href="#">Our Story</a></li>
+                                <li><a href="#">Benefits</a></li>
+                                <li><a href="#">Team</a></li>
+                                <li><a href="#">Carrers</a></li>
+                            </ul>
+                        </section>
+                        <section class="section">
+                            <ul>
+                                <caption>Follow Us</caption>
+                                <li><a href="#">Facebook</a></li>
+                                <li><a href="#">Instagram</a></li>
+                                <li><a href="#">Twitter</a></li>
+                            </ul>
+                        </section>
+                    </span>
+                </div>
+                
+                <div class="bottom">
+                    <span class="rights">
+                        © Computer Programming Society. All Rights Reserved.
+                    </span>
+                    <span class="terms">
+                        <span style="margin-right: 40px;">
+                            Terms & Condition
+                        </span>
+                        <span>
+                            Privacy Policy
+                        </span>
+                    </span>
+                </div>
+            </div>
         </footer>
     </body>
 </html>
