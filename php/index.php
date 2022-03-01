88a26ec0cdfa6c8508e94d98167a5476804ce1da
rchristian <98152145+rchristianandrei@users.noreply.github.com>
2022-03-01 12:59:29 +0800

    Changed directories

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@
     $student_id = $error = '';
 
     if(isset($_POST['submit'])){
-        include 'templates/connection.php';
+        include '../templates/connection.php';
 
         //  Query
         $student_id = $_POST['student_id'];
@@ -24,26 +24,12 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="This is a website used ny CPS to update, manage, and delete data about students.">
-        <title>CPS-Laguna</title>
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <?php include '../templates/head.php' ?>
         <link rel="stylesheet" type="text/css" href="../css/login.css">
     </head>
     <body>
         <header>
-            <img src="../images/cps-logo.png" alt="cps logo" class="logo">
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Events</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-            </nav>
-            <button><a href="index.html">Login</a></button>
+            <?php include '../templates/header.php' ?>
         </header>
         <main>
             <div class="formlayout">
@@ -52,7 +38,7 @@
                     <form action="../php/index.php" method="post">
                         <div><h2>Login</h2></div>
                         <div><label for="student_id">Student ID</label></div>
-                        <div><input type="text" name="student_id" id="student_id" value="<?php echo $student_id; ?>" required></div>
+                        <div><input type="text" name="student_id" id="student_id" value="<?php echo htmlspecialchars($student_id); ?>" required></div>
                         <div><label for="password">Password</label></div>
                         <div><input type="password" name="password" id="password" required></div>
                         <div style="display: flex; justify-content: space-between;">
@@ -72,57 +58,7 @@
             </div>
         </main>
         <footer>
-            <div>
-                <img src="../images/footer-logo.jpg" alt="cps logo" class="logo"> 
-                <hr>
-                <div style="display: flex;">
-                    <span class="news">
-                        <span>Subscribe to our newsletter</span>
-                    </span>
-                    <span class="about">
-                        <section class="section">
-                            <ul>
-                                <caption>Site Map</caption>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">Events</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                            </ul>
-                        </section>
-                        <section class="section">
-                            <ul>
-                                <caption>About</caption>
-                                <li><a href="#">Our Story</a></li>
-                                <li><a href="#">Benefits</a></li>
-                                <li><a href="#">Team</a></li>
-                                <li><a href="#">Carrers</a></li>
-                            </ul>
-                        </section>
-                        <section class="section">
-                            <ul>
-                                <caption>Follow Us</caption>
-                                <li><a href="#">Facebook</a></li>
-                                <li><a href="#">Instagram</a></li>
-                                <li><a href="#">Twitter</a></li>
-                            </ul>
-                        </section>
-                    </span>
-                </div>
-                
-                <div class="bottom">
-                    <span class="rights">
-                        © Computer Programming Society. All Rights Reserved.
-                    </span>
-                    <span class="terms">
-                        <span style="margin-right: 40px;">
-                            Terms & Condition
-                        </span>
-                        <span>
-                            Privacy Policy
-                        </span>
-                    </span>
-                </div>
-            </div>
+            <?php include '../templates/footer.php' ?>
         </footer>
     </body>
 </html>
