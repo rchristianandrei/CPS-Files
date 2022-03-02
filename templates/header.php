e1207d1777613a87bc14999d26fd9b4f26b297b9
rchristian <98152145+rchristianandrei@users.noreply.github.com>
2022-03-02 18:59:31 +0800

    homepage.html -> homepage.php

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <img src="../images/cps-logo.png" alt="cps logo" class="logo">
 <nav>
     <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="../php/homepage.php">Home</a></li>
         <li><a href="../php/students.php">Retrieve</a></li>
         <li><a href="../php/input.php">Add Info</a></li>
         <li><a href="#">Events</a></li>
