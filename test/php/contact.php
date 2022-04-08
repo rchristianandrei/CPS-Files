90fd12ece5b45aa71c3b717f12012afb60442b6a
paolo-sudo <paolodaquioag@gmail.com>
2022-04-08 16:14:16 +0800

    Updated Design of Contact Us Page

--- test/php/contact.php
+++ test/php/contact.php
@@ -24,18 +24,24 @@
             <h2>Contact Us</h2>
         </section>
         <div class="formlayout">
+        <img src="../images/contact/peninsula.jpg" alt="contact photo" class="contact_photo">
             <span class="form">
                 <form action="https://formspree.io/f/xdobzbnk" method="POST" autocomplete="off">
-                    <div><label for="contact_name">Name</label></div>
-                    <div><input type="text" class="textbox" name="contact_name" id="contact_email" placeholder="Enter your name" required></div>
+                    <div><h2>Contact Us</h2></div>
+                    <div><label for="contact_name">Full Name</label></div>
+                    <div><input type="text" class="textbox" name="contact_name" id="contact_email" placeholder="Your Name" required></div>
                     <div><label for="contact_email">Email</label></div>
-                    <div><input type="text" class="textbox" name= "contact_email" id="contact_email" placeholder="email@address.com" required></div>
+                    <div><input type="text" class="textbox" name= "contact_email" id="contact_email" placeholder="Your Email" required></div>
                     <div><label for="subject">Subject</label></div>
-                    <div><input type="text" class="textbox" name= "subject" id="subject" placeholder="Provide context" value="<?php echo $subject; ?>"></div>
+                    <div><input type="text" class="textbox" name= "subject" id="subject" placeholder="Your Subject" value="<?php echo $subject; ?>"></div>
                     <div><label for="message">Message</label></div>
-                    <div><textarea id="message" name="message" class="messagebox" rows="4" cols="50" placeholder="Write your question here" required></textarea></div>
+                    <div><textarea id="message" name="message" class="messagebox" rows="4" cols="50" placeholder="Message" required></textarea></div>
                     <div style="display: flex; justify-content: space-between;"></div>
-                    <center><div><input type="submit" class="messagebutton" value="Send a message"></div></center>
+                    
+                    <div>
+                        <button style="margin-bottom: 20%" type="submit" class="messagebutton" value="Send a message">Send</button>
+                    </div>
+                    
                 </form>
             </span>
         </div>
