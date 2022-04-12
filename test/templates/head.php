85024b1030ff550238148bcb499df26be3ea956e
paolo-sudo <paolodaquioag@gmail.com>
2022-04-12 10:57:46 +0800

    Added space to CPSLaguna -> CPS Laguna

--- test/templates/head.php
+++ test/templates/head.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="This is a website used ny CPS to update, manage, and delete data about students.">
-<title>CPSLaguna</title>
+<title>CPS Laguna</title>
 <link rel="stylesheet" type="text/css" href="../css/header.css">
 <link rel="stylesheet" type="text/css" href="../css/footer.css">
 <link rel="shortcut icon" type="image/png" href="../images/title-logo.png">
